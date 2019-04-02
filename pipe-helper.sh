2c0e2e69712febaf6eb325852cc89d983bd838c1
mk-pmb <mk-pmb@users.noreply.github.com>
2019-04-02 14:18:24 +0200

    rename doc -> docs

--- pipe-helper.sh
+++ pipe-helper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # -*- coding: utf-8, tab-width: 2 -*-
-SELFPATH="$(readlink -m "$BASH_SOURCE"/..)"
-INVOKED_AS="$(basename "$0" .sh)"
 
 
 function pipe_helper () {
+  local SELFPATH="$(readlink -m "$BASH_SOURCE"/..)"
+  local INVOKED_AS="$(basename "$0" .sh)"
   local FLT_CMD=()
   local IN_CMD=()
   case "$INVOKED_AS" in
@@ -12,7 +12,7 @@ function pipe_helper () {
     pipe-noeol )  FLT_CMD=( chardict nl= cr= );;
     pipe-nl2sp )  FLT_CMD=( chardict nl=' ' );;
     show-wsp )    FLT_CMD=( chardict --vocfile
-                    "$SELFPATH/doc/demo/chardict.whitespace.txt" );;
+                    "$SELFPATH/docs/demo/chardict.whitespace.txt" );;
     * ) echo "E: $0: unsupported invocation" >&2; return 1;;
   esac
   IN_CMD+=( "$@" )
