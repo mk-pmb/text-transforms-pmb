9950b18d0ed8c7129251885634f71622adc079fd
mk-pmb <mk-pmb@users.noreply.github.com>
2022-05-29 21:06:34 +0200

    v0.2.1: Explain qrencode-utf8-inverted

--- qrencode-utf8-inverted.sh
+++ qrencode-utf8-inverted.sh
@@ -4,21 +4,33 @@
 
 
 function qrencode_utf8_inverted () {
+  # By default, UTF8 output mode assumes bright on dark, as in traditional
+  # terminal emulator defaults.
+  # This script inverts it, to make the UTF8 image printable dark on bright.
+
+  local SED='
+    s~ |▄|▀|█~<&>~g
+    s~< >~█~g
+    s~<▄>~▀~g
+    s~<▀>~▄~g
+    s~<█>~ ~g
+    '
+
+  while [ "$#" -ge 1 ]; do
+    case "$1" in
+      --padded ) SED+='s~^~:\x1b[7m:  ~; s~$~  :\x1b[0m:  ~; '$'\n';;
+      * ) break;;
+    esac
+    shift
+  done
+
   local QR_OPTS=(
     --level=M
     --8bit
     --dpi=24
     --type=UTF8
     )
-  qrencode "${QR_OPTS[@]}" "$@" | LANG=C sed -rf <(echo '
-    s~ |▄|▀|█~<&>~g
-    s~< >~█~g
-    s~<▄>~▀~g
-    s~<▀>~▄~g
-    s~<█>~ ~g
-    s~^~:\x1b[7m:  ~
-    s~$~  :\x1b[0m:  ~
-    ')
+  qrencode "${QR_OPTS[@]}" "$@" | LANG=C sed -rf <(echo "$SED")
 }
 
 
